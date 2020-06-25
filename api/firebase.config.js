c4a2ef834afa98e2b218dc3c6e25da189b9695c4
disgraceful <noolic97@gmail.com>
2020-06-25 16:28:18 +0300

    Firebase config in separate file

---
+++ api/firebase.config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBJquLDDsi6VJl8kJQ8zRqAHHyGG0SLD9o",
+  authDomain: "bookshelf-a2203.firebaseapp.com",
+  databaseURL: "https://bookshelf-a2203.firebaseio.com",
+  projectId: "bookshelf-a2203",
+  storageBucket: "bookshelf-a2203.appspot.com",
+  messagingSenderId: "942217212778",
+  appId: "1:942217212778:web:2795a60ea9260c875f6b13",
+  measurementId: "G-SV2WRHQ6G2",
+};
--- api/server.js
+++ api/server.js
@@ -4,6 +4,10 @@ import "dotenv/config";
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import routes from "./routes/routes.js";
+import firebase from "firebase/app";
+import { firebaseConfig } from "./firebase.config";
+
 const app = express();
 const port = 4200;
 
@@ -11,20 +15,8 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-import routes from "./routes/routes.js";
 routes(app);
 
-import firebase from "firebase/app";
-var firebaseConfig = {
-  apiKey: "AIzaSyBJquLDDsi6VJl8kJQ8zRqAHHyGG0SLD9o",
-  authDomain: "bookshelf-a2203.firebaseapp.com",
-  databaseURL: "https://bookshelf-a2203.firebaseio.com",
-  projectId: "bookshelf-a2203",
-  storageBucket: "bookshelf-a2203.appspot.com",
-  messagingSenderId: "942217212778",
-  appId: "1:942217212778:web:2795a60ea9260c875f6b13",
-  measurementId: "G-SV2WRHQ6G2",
-};
 firebase.initializeApp(firebaseConfig);
 
 app.all("/*", function (req, res, next) {
