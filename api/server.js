9190b564764855a342aeac2d54cbf15455217c1b
disgraceful <noolic97@gmail.com>
2020-06-25 16:24:29 +0300

    Use webpack for builds now

--- api/server.js
+++ api/server.js
@@ -1,4 +1,6 @@
-import 'dotenv/config';
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import "dotenv/config";
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
@@ -14,27 +16,28 @@ routes(app);
 
 import firebase from "firebase/app";
 var firebaseConfig = {
-    apiKey: "AIzaSyBJquLDDsi6VJl8kJQ8zRqAHHyGG0SLD9o",
-    authDomain: "bookshelf-a2203.firebaseapp.com",
-    databaseURL: "https://bookshelf-a2203.firebaseio.com",
-    projectId: "bookshelf-a2203",
-    storageBucket: "bookshelf-a2203.appspot.com",
-    messagingSenderId: "942217212778",
-    appId: "1:942217212778:web:2795a60ea9260c875f6b13",
-    measurementId: "G-SV2WRHQ6G2"
+  apiKey: "AIzaSyBJquLDDsi6VJl8kJQ8zRqAHHyGG0SLD9o",
+  authDomain: "bookshelf-a2203.firebaseapp.com",
+  databaseURL: "https://bookshelf-a2203.firebaseio.com",
+  projectId: "bookshelf-a2203",
+  storageBucket: "bookshelf-a2203.appspot.com",
+  messagingSenderId: "942217212778",
+  appId: "1:942217212778:web:2795a60ea9260c875f6b13",
+  measurementId: "G-SV2WRHQ6G2",
 };
 firebase.initializeApp(firebaseConfig);
 
-app.all('/*', function (req, res, next) {
-    // CORS headers
-    res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    // Set custom headers for CORS
-    res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
-    if (req.method == 'OPTIONS') {
-        res.status(200).end();
-    } else {
-        next();
-    }
+app.all("/*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-type,Accept,X-Access-Token,X-Key"
+  );
+  if (req.method == "OPTIONS") {
+    res.status(200).end();
+  } else {
+    next();
+  }
 });
 app.listen(port, () => console.log(`Started on port ${port}!`));
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const webpack = require("webpack");
+const nodeExternals = require("webpack-node-externals");
+module.exports = {
+  entry: {
+    server: "./api/server.js",
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    publicPath: "/",
+    filename: "[name].js",
+  },
+  target: "node",
+  node: {
+    __dirname: false,
+    __filename: false,
+  },
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
