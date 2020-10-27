f7e93f81fed3a5ab2c2ffe6af633cb24afe1104d
disgraceful <noolic97@gmail.com>
2020-10-27 10:50:20 +0200

    build fix

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, ""),
     publicPath: "/",
-    filename: "serber-build.js",
+    filename: "server-build.js",
   },
   target: "node",
   node: {
